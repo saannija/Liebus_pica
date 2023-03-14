6309b36f8502b5df4acdeae6949e7c2eecdff0ab
Liebus <sannija@inbox.lv>
2023-03-14 09:02:09 +0200

    ieviestas masivu izveles picam un cilveka datiem

--- src/Liebus_pica.java
+++ src/Liebus_pica.java
@@ -1,20 +1,49 @@
 import javax.swing.JOptionPane;
 
 public class Liebus_pica {
-
+	static double delivery_cena=0;
+	static String[] adrese={"Ziemelu priekspils.","Karosta","Tosmare",
+			"Zala birze","Jaunliepaja","Vecliepaja","Jauna pasaule",
+			"Dienvdrietumi","Ezerkrasts","Arpus pilsetas"};
+	static int[] att={1,3,2,
+			3,1,3,2,
+			5,4,7};
+	
+	static String pasutit[]={"To-go/pasutijums", "Uz vietas"};
+	static String picas[]={"Siera","Tropiska","Studentu","Margarita","Galas","Asa","Peperoni"};
+	static String piedevas[]={"Siers","Bekons","Ananasi","Jalapeno","Zalumi"};
+	static int picu_izm[]={15,20,30,50};
+	static String merces[]={"BBQ","Ranch","Kecups","Saldskaba","Asa"};
+	static int talrunis;
+	static String vards;
+	
+	
+	static void pasutijums(boolean delivery){
+		if(delivery==false){
+			delivery_cena=0;
+		}else{
+			
+		}
+	}
+	
 	public static void main(String[] args) {
+		
 		String izvele, pasut;
 		String darbibas[]={"Pasutit","Iznemt picu","Aizvert"};
-		String pasutit[]={"To-go/pasutijums", "Uz vietas"};
+		
+		boolean delivery=false;
+		
 		izvele=(String)JOptionPane.showInputDialog(null, "Izvelies darbibu","Darbibas",JOptionPane.INFORMATION_MESSAGE,null,darbibas,darbibas[0]);
 		
 		switch(izvele){
 		case "Pasutit":
 		pasut=(String)JOptionPane.showInputDialog(null, "Izvelies uz kurieni ir pasutijums","Darbibas",JOptionPane.INFORMATION_MESSAGE,null,pasutit,pasutit[0]);
 		if(pasut==pasutit[0]){
-			
+			delivery=true;
+			pasutijums(delivery);
 		}else{
-			
+			delivery=false;
+			pasutijums(delivery);
 		}
 		break;
 		case "Iznemt picu":
@@ -24,7 +53,9 @@ public class Liebus_pica {
 			JOptionPane.showMessageDialog(null,"Programma aptureta","Warning",JOptionPane.WARNING_MESSAGE);
 			System.exit(0);
 		break;
+		default:
+			JOptionPane.showMessageDialog(null, "Sada darbiba neeksiste","Warning",JOptionPane.WARNING_MESSAGE);
 		}
-		JOptionPane.showInputDialog("");
 	}
 }
+
