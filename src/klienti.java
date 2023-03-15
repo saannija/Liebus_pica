e60c0398bac5aebc61a48742dc92e08f98e3f85b
Liebus <sannija@inbox.lv>
2023-03-15 08:10:09 +0200

    izveidots klientu objekts ar get/set

--- src/klienti.java
+++ src/klienti.java
@@ -1,9 +1,38 @@
 
 public class klienti {
 
-	public static void main(String[] args) {
-		
+	private String vards, tlf, adrese;
 
+	public klienti(String vards, String tlf, String adrese) {
+		super();
+		this.vards = vards;
+		this.tlf = tlf;
+		this.adrese = adrese;
 	}
+
+	public String getVards() {
+		return vards;
+	}
+
+	public void setVards(String vards) {
+		this.vards = vards;
+	}
+
+	public String getTlf() {
+		return tlf;
+	}
+
+	public void setTlf(String tlf) {
+		this.tlf = tlf;
+	}
+
+	public String getAdrese() {
+		return adrese;
+	}
+
+	public void setAdrese(String adrese) {
+		this.adrese = adrese;
+	}
+	
 
 }
