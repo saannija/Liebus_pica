cfc1926f0ac8f124442f60f56746ed880ec3dd54
Liebus <sannija@inbox.lv>
2023-03-15 08:19:46 +0200

    izveidots ui, ieliktas pamata metodes

--- src/UI.java
+++ src/UI.java
@@ -1,9 +1,20 @@
+import javax.swing.JOptionPane;
 
-public class UI {
-
-	public static void main(String[] args) {
+	public class UI implements pasutijums{
 		
-
+		
+	
+	
+	public static void main(String[] args) {
+		UI ui=new UI();
+		ui.prompt();
+	 }
+	
+	public void prompt(){
+		
 	}
-
+	
+	public void picaIzcepta(picas pica){
+		JOptionPane.showMessageDialog(null, pica.getNos()+" pica ir izcepta, cena "+pica.getCena()+"€\n");//\n Klients: "+klients.getVards()+" Nr. "+klients.getTlf()+" Adrese: "+klients.getAdrese());
+	}
 }
